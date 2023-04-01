3431e2bf40821ef6114a6931b320123c75680b0c
hantsy <hantsy@gmail.com>
2023-04-01 14:30:54 +0800

    chore: clean codes

--- api/src/main/kotlin/com/example/demo/interfaces/dto/CreatePostCommand.kt
+++ api/src/main/kotlin/com/example/demo/interfaces/dto/CreatePostCommand.kt
@@ -1,6 +1,6 @@
 package com.example.demo.interfaces.dto
 
-import javax.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotBlank
 
 data class CreatePostCommand(
     @NotBlank val title: String,
--- api/src/main/kotlin/com/example/demo/interfaces/dto/UpdatePostCommand.kt
+++ api/src/main/kotlin/com/example/demo/interfaces/dto/UpdatePostCommand.kt
@@ -1,6 +1,6 @@
 package com.example.demo.interfaces.dto
 
-import javax.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotBlank
 
 data class UpdatePostCommand(
     @NotBlank val title: String,
