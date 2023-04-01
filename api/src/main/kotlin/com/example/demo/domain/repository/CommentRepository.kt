2213199dfed59595b1ba171c23d457baae65d869
hantsy <hantsy@gmail.com>
2023-04-01 14:40:12 +0800

    chore: clean codes

--- api/src/main/kotlin/com/example/demo/domain/repository/CommentRepository.kt
+++ api/src/main/kotlin/com/example/demo/domain/repository/CommentRepository.kt
@@ -1,10 +1,12 @@
 package com.example.demo.domain.repository
 
 import com.example.demo.domain.model.Comment
-import kotlinx.coroutines.flow.Flow
-import org.springframework.data.repository.kotlin.CoroutineSortingRepository
 import java.util.*
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.data.repository.kotlin.CoroutineSortingRepository
 
-interface CommentRepository : CoroutineSortingRepository<Comment, UUID> {
+interface CommentRepository :
+        CoroutineSortingRepository<Comment, UUID>, CoroutineCrudRepository<Comment, UUID> {
     fun findByPostId(id: UUID): Flow<Comment>
 }
--- api/src/main/kotlin/com/example/demo/domain/repository/PostRepository.kt
+++ api/src/main/kotlin/com/example/demo/domain/repository/PostRepository.kt
@@ -1,13 +1,15 @@
 package com.example.demo.domain.repository
 
-import com.example.demo.interfaces.dto.PostSummary
 import com.example.demo.domain.model.Post
 import com.example.demo.domain.model.Status
+import com.example.demo.interfaces.dto.PostSummary
+import java.util.*
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import java.util.*
+import org.springframework.data.repository.kotlin.CoroutineSortingRepository
 
-interface PostRepository : CoroutineCrudRepository<Post, UUID> {
+interface PostRepository :
+        CoroutineCrudRepository<Post, UUID>, CoroutineSortingRepository<Post, UUID> {
     fun findByTitleContains(title: String): Flow<PostSummary>
     fun findByStatus(status: Status): Flow<Post>
 }
